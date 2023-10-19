12a039918c9dd8183843f62ba53b7fb3e76b5423
dasha280642 <dasha19982009@rambler.ru>
2023-10-20 02:58:04 +0300

    Yes

--- homework-1/main.py
+++ homework-1/main.py
@@ -4,15 +4,15 @@ if __name__ == '__main__':
     item1 = Item("Смартфон", 10000, 5)
     item2 = Item("Ноутбук", 20000, 5)
 
-    print(item1.get_total_cost())  # 200000
-    print(item2.get_total_cost())  # 100000
+    print(item1.get_total_cost())  
+    print(item2.get_total_cost())  
 
     # устанавливаем новый уровень цен
     Item.pay_rate = 0.8
     # применяем скидку
     item1.apply_discount()
 
-    print(item1.price)  # 8000.0
-    print(item2.price)  # 20000
+    print(item1.price)
+    print(item2.price)
 
     print(Item.apply_discount)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
