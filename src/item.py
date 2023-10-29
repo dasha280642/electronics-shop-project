62ac9b8ec05ec05701c36500a7c3be5b347f44c5
dasha280642 <dasha19982009@rambler.ru>
2023-10-29 22:14:00 +0300

    t

--- src/item.py
+++ src/item.py
@@ -3,13 +3,35 @@ class Item:
     instances = []
 
     def __init__(self, name, price, quantity):
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.__class__.instances.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) <= 10:
+            self.__name = value
+        else:
+            self.__name = value[:10]
 
     def get_total_cost(self):
         return self.price * self.quantity
 
     def apply_discount(self):
         self.price = self.price * self.__class__.discount_level
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('src/items.csv', 'r') as file:
+            for line in file:
+                name, price, quantity = line.strip().split(',')
+                cls(name, float(price), int(quantity))
+
+    @staticmethod
+    def string_to_number(string):
+        return float(string)
--- tests/test_item.py
+++ tests/test_item.py
@@ -8,3 +8,28 @@ def test_apply_discount():
     item = Item("Телефон", 10000, 5)
     item.apply_discount()
     assert item.price == 8500
+
+
+def test_instantiate_from_csv():
+    item1 = Item("Item1", 10.0, 2)
+    item2 = Item("Item2", 20.0, 3)
+    item3 = Item("Item3", 30.0, 4)
+
+    Item.instantiate_from_csv()
+
+    assert len(Item.instances) == 6
+    assert Item.instances[3].name == "Item4"
+    assert Item.instances[3].price == 40.0
+    assert Item.instances[3].quantity == 5
+    assert Item.instances[4].name == "Item5"
+    assert Item.instances[4].price == 50.0
+    assert Item.instances[4].quantity == 6
+    assert Item.instances[5].name == "Item6"
+    assert Item.instances[5].price == 60.0
+    assert Item.instances[5].quantity == 7
+def test_string_to_number():
+    assert Item.string_to_number("10.0") == 10.0
+    assert Item.string_to_number("20.5") == 20.5
+    assert Item.string_to_number("30") == 30.0
+    assert Item.string_to_number("-5.5") == -5.5
+    assert Item.string_to_number("0") == 0.0
